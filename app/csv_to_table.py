b8802210aa823a291d068f6b14681dddbda315ea
Michael Lynch <git@mtlynch.io>
2019-11-27 15:04:44 -0500

    Adding logic for rendering markdown tables

---
+++ app/csv_to_table.py
@@ -0,0 +1,98 @@
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
+
+def convert(a, b):
+    rows = []
+    rows.append([
+        'Metric',
+        _format_month(a['month']),
+        _format_month(b['month']), 'Change'
+    ])
+    rows.append(_integer_row('Unique Visitors', 'unique_visitors', a, b))
+    rows.append(_integer_row('Total Pageviews', 'total_pageviews', a, b))
+    rows.append(
+        _integer_row('Domain Authority (Moz)', 'domain_authority_moz', a, b))
+    rows.append(
+        _integer_row('Ranking Keywords (Moz)', 'ranking_keywords_moz', a, b))
+    rows.append(_dollar_amount_row('AdSense Earnings', 'adsense_earnings', a,
+                                   b))
+    rows.append(
+        _dollar_amount_row('Amazon Affiliate Earnings',
+                           'amazon_affiliate_earnings', a, b))
+    rows.append(_dollar_amount_row('Meal Plan Sales', 'meal_plan_sales', a, b))
+    a['total_earnings'] = sum(
+        filter(None, (a['adsense_earnings'], a['amazon_affiliate_earnings'],
+                      a['meal_plan_sales'])))
+    b['total_earnings'] = sum(
+        filter(None, (b['adsense_earnings'], b['amazon_affiliate_earnings'],
+                      b['meal_plan_sales'])))
+    rows.append(_dollar_amount_row('Total Earnings', 'total_earnings', a, b))
+
+    return rows
+
+
+def _format_month(dt):
+    return dt.strftime('%B %Y')
+
+
+def _integer_row(metric_name, key, a, b):
+    return [
+        metric_name,
+        _format_integer(a[key]),
+        _format_integer(b[key]),
+        _format_integer_difference(a[key], b[key])
+    ]
+
+
+def _format_integer_difference(a, b):
+    if not a or not b:
+        return 'N/A'
+    difference = b - a
+    percentage = 100.0 * (float(difference) / a)
+    if difference > 0:
+        sign = '+'
+    elif difference < 0:
+        sign = ''
+    else:
+        return '0'
+    formatted_difference = _format_integer(difference)
+    return '%s%s (%s%.0f%%)' % (sign, formatted_difference, sign, percentage)
+
+
+def _format_integer(i):
+    if not i:
+        return 'N/A'
+    return "{0:n}".format(i)
+
+
+def _dollar_amount_row(metric_name, key, a, b):
+    return [
+        metric_name,
+        _format_dollar_amount(a[key]),
+        _format_dollar_amount(b[key]),
+        _format_dollar_difference(a[key], b[key])
+    ]
+
+
+def _format_dollar_difference(a, b):
+    if not a or not b:
+        return 'N/A'
+    difference = b - a
+    percentage = 100.0 * (difference / a)
+    if difference > 0:
+        sign = '+'
+    elif difference < 0:
+        sign = ''
+    else:
+        return '0'
+    formatted_difference = _format_dollar_amount(difference)
+    formatted_difference = formatted_difference.replace('$-', '-$')
+    return '%s%s (%s%.0f%%)' % (sign, formatted_difference, sign, percentage)
+
+
+def _format_dollar_amount(d):
+    if not d:
+        return 'N/A'
+    return '$%.2f' % d
---
+++ app/markdown_table.py
@@ -0,0 +1,83 @@
+def generate(table):
+    formatted_table = table
+    formatted_table = _add_change_formatting(formatted_table)
+    formatted_table = _add_total_summary_formatting(formatted_table)
+
+    column_widths = _find_column_widths(formatted_table)
+    formatted_table = _add_padding(formatted_table, column_widths)
+
+    lines = []
+    lines.append(_make_row(formatted_table[0]))
+    lines.append(_make_header_separators(column_widths))
+    for row in formatted_table[1:]:
+        lines.append(_make_row(row))
+    return '\n'.join(lines)
+
+
+def _add_change_formatting(table):
+    rows = []
+    rows.append(table[0])
+    for row in table[1:]:
+        columns = []
+        for i, column in enumerate(row):
+            if i == len(row) - 1:
+                if column.startswith('-'):
+                    columns.append('<font color="red">%s</font>' % column)
+                elif column.startswith('+'):
+                    columns.append('<font color="green">%s</font>' % column)
+                else:
+                    columns.append(column)
+            else:
+                columns.append(column)
+        rows.append(columns)
+    return rows
+
+
+def _add_total_summary_formatting(table):
+    rows = table[:-1]
+    final_row = []
+    for value in table[-1]:
+        final_row.append('**%s**' % value)
+    rows.append(final_row)
+    return rows
+
+
+def _add_padding(table, column_widths):
+    rows = []
+    for unformatted_row in table:
+        row = []
+        for column_index, value in enumerate(unformatted_row):
+            if len(value) >= column_widths[column_index]:
+                row.append(value)
+            else:
+                padding = ' ' * (column_widths[column_index] - len(value))
+                row.append(value + padding)
+        rows.append(row)
+    return rows
+
+    rows = table[:-1]
+    final_row = []
+    for value in table[-1]:
+        final_row.append('**%s**' % value)
+    rows.append(final_row)
+    return rows
+
+
+def _find_column_widths(table):
+    column_count = len(table[0])
+    widths = [0] * column_count
+    for row in table:
+        for i, column in enumerate(row):
+            widths[i] = max(widths[i], len(column))
+    return widths
+
+
+def _make_header_separators(column_widths):
+    separators = []
+    for width in column_widths:
+        separators.append('-' * width)
+    return _make_row(separators)
+
+
+def _make_row(row):
+    return '| ' + ' | '.join(row) + ' |'
