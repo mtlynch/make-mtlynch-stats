d374d2a002a7cb7b0393f10f8a21456b24e4d79d
Michael Lynch <git@mtlynch.io>
2020-01-02 11:23:46 -0500

    Adding support for Zestful stats

--- tests/test_parse_csv.py
+++ tests/test_parse_csv.py
@@ -10,7 +10,7 @@ class ParseCsvTest(unittest.TestCase):
     def setUp(self):
         self.maxDiff = None
 
-    def test_parse(self):
+    def test_parse_is_it_keto_csv(self):
         dummy_file = io.StringIO("""
 Month,Unique Visitors,Total Pageviews,Domain Authority (Moz),Ranking Keywords (Moz),Domain Rating (Ahrefs),AdSense Earnings,Amazon Affiliate Earnings,Meal Plan Sales,Total Earnings
 2019-9,"28,768","75,487",10,"2,330",N/A,$178.79,$150.06,N/A,$328.85
@@ -40,5 +40,30 @@ Month,Unique Visitors,Total Pageviews,Domain Authority (Moz),Ranking Keywords (M
                 'amazon_affiliate_earnings': 159.02,
                 'meal_plan_sales': 23.87,
                 'total_earnings': 258.54,
+            },
+        ], parse_csv.parse(dummy_file))
+
+    def test_parse_zestful_csv(self):
+        dummy_file = io.StringIO("""
+Month,Unique Visitors,Total Pageviews,RapidAPI Earnings,Enterprise Plan Earnings,Total Earnings,RapidAPI Earnings (after fees),Enterprise Plan Earnings (after fees)
+2019-11,232,320,$81.66,$0.00,$65.33,$65.33,$0.00
+2019-12,207,594,$65.30,"$4,000.00","$3,935.94",$52.24,"$3,883.70"
+""".strip())
+        self.assertEqual([
+            {
+                'month': datetime.date(year=2019, month=11, day=1),
+                'unique_visitors': 232,
+                'total_pageviews': 320,
+                'rapidapi_earnings': 65.33,
+                'enterprise_plan_earnings': 0.0,
+                'total_earnings': 65.33,
+            },
+            {
+                'month': datetime.date(year=2019, month=12, day=1),
+                'unique_visitors': 207,
+                'total_pageviews': 594,
+                'rapidapi_earnings': 52.24,
+                'enterprise_plan_earnings': 3883.7,
+                'total_earnings': 3935.94,
             },
         ], parse_csv.parse(dummy_file))
