0b342873dc678fdbb50a7d3cd96cdbc5b84ddd06
Michael Lynch <git@mtlynch.io>
2020-11-20 18:29:09 -0500

    Add support for dumping JSON revenue data

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import argparse
 import logging
 import sys
 
+import csv_to_json
 import csv_to_markdown_table
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +25,10 @@ def configure_logging():
 def main(args):
     configure_logging()
     logger.info('Started runnning')
-    print(csv_to_markdown_table.convert(sys.stdin, args.offset))
+    if not args.to_json:
+        print(csv_to_markdown_table.convert(sys.stdin, args.offset))
+    else:
+        print(csv_to_json.convert(sys.stdin))
 
 
 if __name__ == '__main__':
@@ -31,4 +36,5 @@ if __name__ == '__main__':
         prog='Generate mtlynch Stats',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--offset', default=-1, type=int)
+    parser.add_argument('--to-json', action='store_true')
     main(parser.parse_args())
